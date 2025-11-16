80ad6095215feb4d3cdce71a565ac29be98374fc
Pyrospower <thepassione7@gmail.com>
2025-11-16 22:28:16 +0100

    Update import sorting config
    - Removed Tailwind config file
    - Bumped TypeScript version to 5.0.0

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -24,10 +24,9 @@ const config = {
     },
   ],
   astroAllowShorthand: false,
-  tailwindConfig: "./tailwind.config.ts",
   importOrder: ["<THIRD_PARTY_MODULES>", "", "^~/", "^[.][.]/", "^[.]/"],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
+  importOrderTypeScriptVersion: "5.0.0",
 };
 
 export default config;
